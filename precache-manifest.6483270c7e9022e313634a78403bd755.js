260fb2d3cc13e6d62e887cf8f3c73a3189f160b6
ElvinsYan <yanxiangyang94@gmail.com>
2022-03-08 23:00:47 +0800

    deploy

---
+++ precache-manifest.6483270c7e9022e313634a78403bd755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d286c3aecfdfa1ad1f98",
+    "url": "/iTally-vue-website/css/app.2b6fa66d.css"
+  },
+  {
+    "revision": "8e8cb6ff3665aad6c480b3f4287c672c",
+    "url": "/iTally-vue-website/index.html"
+  },
+  {
+    "revision": "d286c3aecfdfa1ad1f98",
+    "url": "/iTally-vue-website/js/app.58fbda4a.js"
+  },
+  {
+    "revision": "d5861ddc05b5e00d5c80",
+    "url": "/iTally-vue-website/js/chunk-vendors.a5f4c7bf.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/iTally-vue-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/iTally-vue-website/robots.txt"
+  }
+]);
